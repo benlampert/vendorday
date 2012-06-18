ea3f2ccc65baf7dfa9518076f9f8b8a3ee1277fc
Ben Lampert <benlampert@gmail.com>
2012-06-17 20:25:09 -0400

    adding tumblelog with mongolab

--- tumblelog/tumblelog.py
+++ tumblelog/tumblelog.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flaskext.mongoengine import MongoEngine
+from mongoengine import connect
+
+connect('tumbleblog_mlab', host='mongodb://benlampert:hannah@ds033877.mongolab.com:33877/flasktest')
+
+#mongodb://benlampert:hannah24@ds033877.mongolab.com:33877/flasktest
 
 app = Flask(__name__)
-app.config["MONGODB_DB"] = "my_tumble_log"
+app.config["MONGODB_DB"] = "flasktest"
 
 db = MongoEngine(app)
+#db = mongoengine.connect('flasktest', host='ds033877.mongolab.com:33877', username='benlampert', password='hannah24')
 
 def register_blueprints(app):
 	#Prevents circular imports
