2a75b3c96e737eea95ad2b7ce71c2a3728ace961
Ben Lampert <benlampert@gmail.com>
2012-06-18 11:58:18 -0300

    removed username and password from URI since this is public

--- tumblelog/tumblelog.py
+++ tumblelog/tumblelog.py
@@ -2,15 +2,16 @@ from flask import Flask
 from flaskext.mongoengine import MongoEngine
 from mongoengine import connect
 
-connect('tumbleblog_mlab', host='mongodb://benlampert:hannah@ds033877.mongolab.com:33877/flasktest')
+un = ''
+pw = ''
 
-#mongodb://benlampert:hannah24@ds033877.mongolab.com:33877/flasktest
+connect('tumbleblog_mlab', host='mongodb://'+un+':'+pw+'@ds033877.mongolab.com:33877/flasktest')
+
 
 app = Flask(__name__)
 app.config["MONGODB_DB"] = "flasktest"
 
 db = MongoEngine(app)
-#db = mongoengine.connect('flasktest', host='ds033877.mongolab.com:33877', username='benlampert', password='hannah24')
 
 def register_blueprints(app):
 	#Prevents circular imports
